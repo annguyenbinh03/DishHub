d7c8139aea312497d35e244fd795110ba14b1330
annguyenbinh03 <phaolobinhan@gmail.com>
2025-03-14 00:35:37 +0700

    choose restaurant in layout

--- src/layouts/AdminLayout/NavBar/NavLeft/index.jsx
+++ src/layouts/AdminLayout/NavBar/NavLeft/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ListGroup, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import useWindowSize from '../../../../hooks/useWindowSize';
 import NavSearch from './NavSearch';
+import useAuth from 'hooks/useAuth';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const NavLeft = () => {
   const windowSize = useWindowSize();
@@ -12,6 +15,34 @@ const NavLeft = () => {
   if (windowSize.width <= 575) {
     navItemClass = [...navItemClass, 'd-none'];
   }
+
+  const [restaurants, setRestaurants] = useState([]);
+  const [selectedRestaurant, setSelectedRestaurant] = useState('');
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  const { auth } = useAuth();
+
+  const fetchRestaurants = async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${auth.token}`
+      }
+    };
+    try {
+      const response = await axios.get('https://dishub-dxacd4dyevg9h3en.southeastasia-01.azurewebsites.net/api/admin/restaurants', config);
+      if (response.isSucess) {
+        console.log(response.data);
+        setRestaurants(response.data);
+      } else {
+        toast.error('Lấy danh sách nhà hàng thất bại!');
+      }
+    } catch (error) {
+      toast.error('Có lỗi xảy ra!');
+    }
+  };
 
   return (
     <React.Fragment>
@@ -44,6 +75,23 @@ const NavLeft = () => {
         </ListGroup.Item>
         <ListGroup.Item as="li" bsPrefix=" " className="nav-item">
           <NavSearch windowWidth={windowSize.width} />
+        </ListGroup.Item>
+        <ListGroup.Item as="li" className="nav-item">
+          <Dropdown align={'start'}>
+            <Dropdown.Toggle variant={'link'} id="dropdown-basic">
+              Dropdown
+            </Dropdown.Toggle>
+            <ul>
+              <Dropdown.Menu>
+                <li>
+                  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                </li>
+                <li>
+                  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </li>
+              </Dropdown.Menu>
+            </ul>
+          </Dropdown>
         </ListGroup.Item>
       </ListGroup>
     </React.Fragment>
